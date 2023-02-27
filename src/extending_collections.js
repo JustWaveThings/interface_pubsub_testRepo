1bde7238bc1607dc4e87c80c091eea189829085f
JustWaveThings <justwavethings@proton.me>
2023-02-27 13:09:32 -0500

    branch to examine core functionality

--- src/extending_collections.js
+++ src/extending_collections.js
@@ -7,7 +7,7 @@ const Collection = (title = 'Default Collection', _id = uuidv4()) => {
 	const observers = {};
 
 	const add = (item) => {
-		const thing = Object.freeze({ _id, data: item });
+		const thing = { _id, data: item };
 		stuff = [...stuff, thing];
 		observers.add?.forEach((observerFunc) =>
 			// pass as arguments - wrapped item, collection as pure data
@@ -15,33 +15,12 @@ const Collection = (title = 'Default Collection', _id = uuidv4()) => {
 		);
 		return thing;
 	};
-	const remove = (id) => {
-		stuff = stuff.filter((thing) => thing._id !== id);
-	};
-
-	const findById = (id) => stuff.find((thing) => thing._id === id);
-	const find = (func) => stuff.filter(({ data }) => func(data));
-	const findAll = () => [...stuff];
-	const update = (id, updateFunc) => {
-		stuff = stuff.map((thing) =>
-			thing._id === id
-				? Object.freeze({ _id, data: updateFunc(thing.data) })
-				: thing
-		);
-		return findById(id);
-	};
 
 	const subscribe = (action, observerFunction) => {
 		if (!observers.hasOwnProperty(action)) {
 			observers[action] = [];
 		}
 		observers[action].push(observerFunction);
-	};
-
-	const unsubscribe = (action, observerFunction) => {
-		observers[action] = observers[action].filter(
-			(func) => func !== observerFunction
-		);
 	};
 
 	return {
@@ -52,12 +31,7 @@ const Collection = (title = 'Default Collection', _id = uuidv4()) => {
 			return title;
 		},
 		add,
-		remove,
-		find,
-		findAll,
-		update,
 		subscribe,
-		unsubscribe,
 	};
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import './style.css';
 
-// how to get access to the 'subscribe' method from the Collection() factory function?
 import Collection from './extending_collections';
-import tobyDictionary from './toby_dictionary';
 
-// goal --- to log tobyDictionary to console with the logInUpperCase() method applied to it.
+const tobyDictionary = Collection("Toby's Dictionary");
 
-// where i'm stuck -- not logging anything to console.
+tobyDictionary.add({
+	term: 'Cabbage',
+	definition:
+		"n. A familiar kitchen-garden vegetable about as large and wise as a man's head.",
+});
+tobyDictionary.add({
+	term: 'Cat',
+	definition:
+		'n. A soft, indestructible automaton provided by nature to be kicked when things go wrong in the domestic circle.',
+});
+tobyDictionary.add({
+	term: 'Famous',
+	definition: 'adj. Conspicuously miserable.',
+});
+tobyDictionary.add({
+	term: 'History',
+	definition:
+		'n. An account mostly false, of events mostly unimportant, which are brought about by rulers mostly knaves, and soldiers mostly fools.',
+});
+tobyDictionary.add({
+	term: 'Resident',
+	definition: 'adj. Unable to leave.',
+});
